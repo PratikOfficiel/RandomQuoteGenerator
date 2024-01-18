4071e332d34de06779d1a88f97b5960638eedcfc
Pratik01-git <Pratik01-git>
2024-01-19 02:57:45 +0530

    zenApi sucessfully used using proxy in our own backend

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+import express, { response } from "express";
+import axios from 'axios';
+
+const app = express();
+
+const API_URL = "https://zenquotes.io/api/random/[your_key]";
+
+app.get('/api/quotes', (req, res) => {
+    
+    axios.get(API_URL)
+    .then((response)=>{
+        res.send(response.data);
+    })
+    .catch((err)=>{
+        res.send(`there is some error in the backend ${err}`)
+    })
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
